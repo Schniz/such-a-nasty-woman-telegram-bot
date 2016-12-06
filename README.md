cf8ce7f88fee7062880577272d18e42ec739a487
Gal Schlezinger <galstar@spitfire.co.il>
2016-12-06 16:09:02 +0200

    implementation readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Such a Nasty Woman Telegram Bot
+
+Provides the right GIF to answer nasty people
+
+## Usage
+
+inline bot: `@sanwbot` and press the gif
+command: `/sanw`
+
+cheers
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-require('dotenv').load();
+try {
+  require('dotenv').load();
+} catch(e) { }
 
+const TelegramBot = require('node-telegram-bot-api');
 const { TELEGRAM_BOT_TOKEN } = process.env;
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
-console.log(TELEGRAM_BOT_TOKEN);
+const inlineGif = id => ({
+  type: 'gif',
+  id: '1',
+  gif_file_id: id,
+});
+
+const images = {
+  gif: 'BQADBAADAgADUJumEhgovbLa_EFEAg',
+};
+
+bot.onText(/\/sanw/, function (msg, match) {
+  var chatId = msg.chat.id;
+  console.log('log: answered message command');
+  bot.sendDocument(chatId, images.gif);
+});
+
+bot.on('inline_query', function(msg) {
+  const queryId = msg.id;
+  inlineAnswer = [inlineGif(images.gif)];
+  console.log('log: answered inline query');
+  bot.answerInlineQuery(queryId, inlineAnswer);
+});
