553eadd783614e6a2e37a6d355bdf4be58400051
Gal Schlezinger <galstar@spitfire.co.il>
2017-02-17 19:58:19 +0200

    added status

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ try {
   require('dotenv').load();
 } catch(e) { }
 
+const { createServer } = require('http');
 const TelegramBot = require('node-telegram-bot-api');
 const { TELEGRAM_BOT_TOKEN } = process.env;
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+let numOfEvents = 0;
 
 const inlineGif = id => ({
   type: 'gif',
@@ -17,14 +19,20 @@ const images = {
 };
 
 bot.onText(/\/sanw/, function (msg, match) {
+  numOfEvents++;
   var chatId = msg.chat.id;
   console.log('log: answered message command');
   bot.sendDocument(chatId, images.gif);
 });
 
 bot.on('inline_query', function(msg) {
+  numOfEvents++;
   const queryId = msg.id;
   inlineAnswer = [inlineGif(images.gif)];
   console.log('log: answered inline query');
   bot.answerInlineQuery(queryId, inlineAnswer);
 });
+
+createServer((req, res) => {
+  res.end(`<h1>working here!</h1>number of events: ${numOfEvents}`)
+}).listen(process.env.PORT || 8001)
